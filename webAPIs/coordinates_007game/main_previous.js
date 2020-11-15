5930df2046d6dd02acad304375bcb7de09bb5152
Dylan <kaosanct0@gmail.com>
2020-11-15 17:32:15 +0700

    improve performance

--- webAPIs/coordinates_007game/main_previous.js
+++ webAPIs/coordinates_007game/main_previous.js
@@ -5,30 +5,26 @@ const horizontal = document.querySelector(".horizontal");
 const vertical = document.querySelector(".vertical");
 const target = document.querySelector(".target");
 
-addEventListener('load',() => {
+addEventListener('load',()=>{
   const targetRect = target.getBoundingClientRect();
   const targetHalfWidth = targetRect.width / 2;
   const targetHalfHeight = targetRect.height / 2;
   document.addEventListener("mousemove", (e) => {
     const x = e.clientX;
     const y = e.clientY;
-    // vertical.style.left = `${x}px`;
-    // horizontal.style.top = `${y}px`;
-    // target.style.left = `${x}px`;
-    // target.style.top = `${y}px`;
-    // tag.innerText = `(${e.clientX},${e.clientY})`;
-    // tag.style.top = `${y}px`;
-    // tag.style.left = `${x}px`;
+    vertical.style.left = `${x}px`;
+    horizontal.style.top = `${y}px`;
+    target.style.left = `${x}px`;
+    target.style.top = `${y}px`;
+    tag.innerText = `(${e.clientX},${e.clientY})`;
+    tag.style.top = `${y}px`;
+    tag.style.left = `${x}px`;
 
     //top, left 를 업데이트 시키는건 계속해서 layout, print, composition 과정이 일어나기때문에 성능이 좋지않음.
     // composition만 일어나는 transform을 사용하는게 효율적.
     // 참고 : https://csstriggers.com/
 
-
-    vertical.style.transform = `translateX(${x}px)`;
-    horizontal.style.transform = `translateY(${y}px)`;
-    target.style.transform = `translate(${x - targetHalfWidth}px,${y - targetHalfHeight}px)`
-    tag.style.transform = `translate(${x + 20}px, ${y + 20}px)`;
-    tag.innerText = `(${x}px,${y}px)`;
   });
+
+
 });
