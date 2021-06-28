7c0629f6aa5bb1c89b888364a4fbc9863a25c9bd
Dylan <kaosanct0@gmail.com>
2021-06-28 20:01:01 +0700

    adding main.js

---
+++ mini-game/main.js
@@ -0,0 +1,165 @@
+'use strict';
+
+const CARROT_SIZE = 80;
+const CARROT_COUNT = 5;
+const BUG_COUNT = 5;
+const GAME_DURATION_SEC = 5;
+
+const field = document.querySelector('.game__field');
+const fieldRect = field.getBoundingClientRect(); 
+const gameBtn = document.querySelector('.game__button');
+const gameTimer = document.querySelector('.game__timer');
+const gameScore = document.querySelector('.game__score');
+
+const popUp = document.querySelector('.pop-up');
+const popUpText = document.querySelector('.pop-up__message');
+const popUpRefresh = document.querySelector('.pop-up__refresh');
+
+let started = false;
+let score = 0;
+let timer = undefined;
+
+field.addEventListener('click',onFieldClick);
+
+gameBtn.addEventListener('click',()=>{
+    if(started){
+        stopGame();
+    }else {
+        startGame();
+    }
+
+});
+
+popUpRefresh.addEventListener('click',()=>{
+    startGame();
+    hidePopup();
+});
+
+
+function startGame(){
+    started = true;
+    initGame();   
+    showStopButton();
+    showTimerAndScore();
+    startGameTimer();
+}
+
+function stopGame(){
+    started = false;
+    stopGameTimer();
+    hideGameButton();
+    showPopupWithText('REPLAY ?');
+}
+
+function finishGame(win){
+    started = false;
+    hideGameButton();
+    stopGameTimer();
+    showPopupWithText(win? 'YOU WON🐶':'YOU LOST💀');
+}
+
+
+function showStopButton(){
+    const icon = gameBtn.querySelector('.fas');
+    icon.classList.add('fa-stop');
+    icon.classList.remove('fa-play');
+    gameBtn.style.visibility = 'visible';
+}
+
+function hideGameButton(){
+    gameBtn.style.visibility = 'hidden';
+}
+
+function showTimerAndScore(){
+    gameTimer.style.visibility = 'visible';
+    gameScore.style.visibility = 'visible';
+}
+
+function startGameTimer(){
+    let remainingTimeSec = GAME_DURATION_SEC;
+    updateTimerText(remainingTimeSec);
+    timer = setInterval(() => {
+        if(remainingTimeSec <= 0){
+            clearInterval(timer);
+            finishGame(CARROT_COUNT === score);
+            return;
+        }
+        updateTimerText(--remainingTimeSec);
+    }, 1000);
+}
+
+function stopGameTimer(){
+    clearInterval(timer);
+}
+
+function updateTimerText(time){
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    gameTimer.innerText = `${minutes}:${seconds}`;
+}
+
+function showPopupWithText(text){
+    popUpText.innerText = text;
+    popUp.classList.remove('pop-up--hide');
+}
+
+function hidePopup(){
+    popUp.classList.add('pop-up--hide');
+}
+
+
+function initGame(){
+    score = 0;
+    field.innerHTML='';
+    gameScore.innerText = CARROT_COUNT;
+    // 벌레, 당근 생성후 field에 추가
+    addItem('carrot',CARROT_COUNT,'img/carrot.png');
+    addItem('bug',BUG_COUNT,'img/bug.png');
+}
+
+function onFieldClick(event){
+    if(!started){
+        return;
+    }
+    const target = event.target;
+    if(target.matches('.carrot')){
+        //당근
+        target.remove();
+        score++;
+        updateScoreBoard();
+        if(score === CARROT_COUNT){
+            finishGame(true);
+        }
+    } else if (target.matches('.bug')){
+        //벌레
+        stopGameTimer();
+        finishGame(false);
+    }
+}
+
+
+function updateScoreBoard(){
+    gameScore.innerText = CARROT_COUNT - score;
+}
+
+function addItem(className, count, imgPath){
+    const x1 = 0;
+    const y1 = 0;
+    const x2 = fieldRect.width - CARROT_SIZE;
+    const y2 = fieldRect.height - CARROT_SIZE;
+    for(let i = 0; i < count; i++){
+        const item = document.createElement('img');
+        item.setAttribute('class',className);
+        item.setAttribute('src',imgPath);
+        item.style.position = 'absolute';
+        const x = randomNumber(x1,x2);
+        const y = randomNumber(y1,y2);
+        item.style.left = `${x}px`;
+        item.style.top = `${y}px`;
+        field.appendChild(item);
+    }
+}
+
+function randomNumber(min,max){
+    return Math.random() * (max-min) + min;
+}
